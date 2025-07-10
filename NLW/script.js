00f2d41d2d63b433597afb0b7af519e2fbaefb64
Larissa Valli <larissavalli4@gmail.com>
2025-07-10 19:09:16 -0300

    terminado

--- NLW/script.js
+++ NLW/script.js
@@ -5,18 +5,54 @@ const askButton = document.getElementById('askButton')
 const aiResponse = document.getElementById('aiResponse')
 const form = document.getElementById('form')
 
+
+const markdownToHTML = (text) => {
+    const converter = new showdwn.Converter()
+    return converter.makeHtml(text)
+}
+
 const perguntarAI = async (question, game, apiKey) ==> {
     const model = "gemini-2.0-flash"
     const geminiURL = `https://generativelanguage.googleapis.com/v1beta/models/${model}:generateContent?key=${apiKey}`
     const pergunta = `
-    olha, tenho esse jogo ${game} e queria saber ${question}
+    ##Especialidade
+    Você é um assistente de meta para o jogo ${game}
+
+
+    ##tarefa
+    Você deve responder as perguntas do usuário com base no seu conhecimento do jogo, estratégias, build e dicas
+
+    ##Regras
+    - Se você nao sabe a resposta, responda com 'Não sei' e não tende inventar uma resposta.
+    - Se a pergunta não está relacionada ao jogo, responda com 'Essa pergunta não está relacionada ao jogo'
+    - Considere a data atual ${new Date().toLocaleDateString()}
+    - Faça pesquisas atualizadas sobre o patch atual, baseando na data atual, para dar uma resposta coerente.
+    - Nunca responda itens que você não tenha certeza de que existe no patch atual.
+
+    ##Resposta
+    - Economize na resposta, seja direto e responda no máximo 500 caracteres
+    - Responda em markdown
+    - Não precisa fazer nenhuma saudação ou despedida, apenas responda o que o usuário está querendo.
+    
+    ##Exemplo de resposta
+    Pergunta do usuário: Melhor build rengar jungle
+    resposta: A build mais atual é: \n\n **Itens:**\n\n coloque os itens aqui.\n\n**Runas:**\n\n exemplo de runas\n\n
+
+    ___
+
+    Aqui está a pergunta do usuário: ${question}
     `
 
     const contents = [{
+        role:"user",
         parts: [{
             text: pergunta
         }]
     }]
+
+    const tools = [(
+       google_search: {}
+    )]
 
     //chamada API
     const response = await fetch(geminiURL, {
@@ -25,12 +61,12 @@ const perguntarAI = async (question, game, apiKey) ==> {
             'Content-Type': 'application/json'
         }
         body: JSON.stringify({
-            contents
+            contents,
+            tools
         })
     })
     
     const data = await response.json()
-    return data.candidates[0].content.parts[0].text
 
 }
 
@@ -53,6 +89,8 @@ const enviarFormulario = async (event) => {
     try {
         //perguntar para IA
         const text = await perguntarAI (question, game, apiKey)
+        aiResponse.querySelector('.response-content').innerHTML = markdownToHTML(text)
+        aiResponse.classList.remove('hidden')
 
     } catch(error) {
         console.log('Erro: ', error)
