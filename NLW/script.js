150b91f6188faf9d5e2cce3ee13991d75ef263a3
Larissa Valli <larissavalli4@gmail.com>
2025-07-09 21:56:06 -0300

    colocando IA no site

--- NLW/script.js
+++ NLW/script.js
@@ -5,9 +5,38 @@ const askButtom = document.getElementById('askButton')
 const aiResponse = document.getElementById('aiResponse')
 const form = document.getElementById('form')
 
+const perguntarAI = async () ==> {
+    
+}
+
 
 const enviarFormulario = (event) => {
     event.preventDefault()
+    const apiKey = apiKeyInput.value 
+    const game = gameSelect.value
+    const question = questionInput.value
+
+    console.log({apiKey, game, question})
+
+    if(apiKey == '' || game == '' || question == '') {
+        alert('Por favor, preencha todos os campos')
+        return
+    }
+
+    askButton.disabled = true
+    askButton.textContent = 'Perguntando...'
+    askButton.classList.add('loading')
+
+    try {
+        //perguntar para IA
+    } catch(error) {
+        console.log('Erro: ', error)
+    } finally {
+        askButton.disabled = false 
+        askButton.textContent = "Perguntar"
+        askButton.classList.remove('loading')
+    }
+
     console.log(event)
 }
 form.addEventListener('submit', enviarFormulario)
