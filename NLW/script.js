3a9297a4db978cfeeb7120b35e22463fa006a458
Larissa Valli <larissavalli4@gmail.com>
2025-07-10 16:15:22 -0300

    atualizando algumas coisas

--- NLW/script.js
+++ NLW/script.js
@@ -6,17 +6,41 @@ const aiResponse = document.getElementById('aiResponse')
 const form = document.getElementById('form')
 
 const perguntarAI = async (question, game, apiKey) ==> {
+    const model = "gemini-2.0-flash"
+    const geminiURL = `https://generativelanguage.googleapis.com/v1beta/models/${model}:generateContent?key=${apiKey}`
+    const pergunta = `
+    olha, tenho esse jogo ${game} e queria saber ${question}
+    `
+
+    const contents = [{
+        parts: [{
+            text: pergunta
+        }]
+    }]
+
+    //chamada API
+    const response = await fetch(geminiURL, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+        body: JSON.stringify({
+            contents
+        })
+    })
+
+    const data = await response.json()
+    console.log({ data })
+    return data
 
 }
 
 
-const enviarFormulario = (event) => {
+const enviarFormulario = async (event) => {
     event.preventDefault()
     const apiKey = apiKeyInput.value 
     const game = gameSelect.value
     const question = questionInput.value
-
-    console.log({apiKey, game, question})
 
     if(apiKey == '' || game == '' || question == '') {
         alert('Por favor, preencha todos os campos')
@@ -29,6 +53,7 @@ const enviarFormulario = (event) => {
 
     try {
         //perguntar para IA
+        await perguntarAI (question, game, apiKey)
     } catch(error) {
         console.log('Erro: ', error)
     } finally {
