6223312764cb0bb14af2cfba48018fdb2afc34d2
Larissa Valli <larissavalli4@gmail.com>
2025-07-09 21:28:57 -0300

    atualizando javascript

--- NLW/script.js
+++ NLW/script.js
@@ -5,4 +5,9 @@ const askButtom = document.getElementById('askButton')
 const aiResponse = document.getElementById('aiResponse')
 const form = document.getElementById('form')
 
-form
+
+const enviarFormulario = (event) => {
+    event.preventDefault()
+    console.log(event)
+}
+form.addEventListener('submit', enviarFormulario)
